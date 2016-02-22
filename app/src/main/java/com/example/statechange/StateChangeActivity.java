2932755814e51a1003c3138334826f452a413c7e
zaifrun <martinknud@gmail.com>
2016-02-22 14:43:31 +0100

    changed functionality

--- app/src/main/java/com/example/statechange/StateChangeActivity.java
+++ app/src/main/java/com/example/statechange/StateChangeActivity.java
@@ -4,17 +4,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class StateChangeActivity extends Activity {
 
 	private static final String TAG = "com.example.StateChange";
-	
+	private String name = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state_change);
         Log.i(TAG, "onCreate");
+		//get a reference for our save button from xml
+		Button saveButton = (Button) findViewById(R.id.saveButton);
+		//get a reference to our editText from xml
+		final EditText editName = (EditText) findViewById(R.id.editName);
+
+		//initialize our text field
+		final TextView textView = (TextView) findViewById(R.id.name);
+        textView.setText("Saved Name:"+name);
+
+		//add a click listener to our save button
+		saveButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//change our name variable to the input from the
+				//user
+				name = editName.getText().toString();
+				//set the new value in the text field
+				textView.setText("Saved Name: "+name);
+			}
+		});
     }
 
 	protected void onStart() {
@@ -47,17 +71,18 @@ public class StateChangeActivity extends Activity {
 		Log.i(TAG, "onDestroy");
 	}
 
+	//This method is called before our activity is created
 	protected void onSaveInstanceState(Bundle outState) {
 		//ALWAYS CALL THE SUPER METHOD
 		super.onSaveInstanceState(outState);
 		Log.i(TAG, "onSaveInstanceState");
-		/* Here we put code now to save the state of the edit text */
-		EditText edit = (EditText) findViewById(R.id.editText1);
-		
-		outState.putString("edittext", edit.getText().toString());
+		/* Here we put code now to save the state */
+		outState.putString("savedName", name);
 		
 	}
-	
+	//this is called when our activity is recreated, but
+	//AFTER our onCreate method has been called
+	//EXTREMELY IMPORTANT DETAIL
 	protected void onRestoreInstanceState(Bundle savedState) {
 		//MOST UI elements will automatically store the information
 		//if we call the super.onRestoreInstaceState
@@ -65,11 +90,17 @@ public class StateChangeActivity extends Activity {
 		super.onRestoreInstanceState(savedState);
 		Log.i(TAG, "onRestoreInstanceState");
 		/*Here we restore any state */
-		EditText edit = (EditText) findViewById(R.id.editText1);
+		TextView savedName = (TextView) findViewById(R.id.name);
 		//in the line below, notice key value matches the key from onSaved
-		String text = savedState.getString("edittext"); 
-		//edit.setText(text); //try to leave this line out and see what happens?
-		
+		//this is of course EXTREMELY IMPORTANT
+		this.name = savedState.getString("savedName");
+
+		//since this method is called AFTER onCreate
+		//we need to set the text field
+		//try to comment the line out and
+		//see the effect after orientation change (after saving some name)
+		savedName.setText("Saved Name:"+name);
+
 	}
 
 
