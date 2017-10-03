790d55d9b4482e3662bf3cbf156abfb6e82eca6f
zaifrun <martinknud@gmail.com>
2017-10-03 13:08:26 +0200

    test

--- app/src/main/java/com/example/statechange/StateChangeActivity.java
+++ app/src/main/java/com/example/statechange/StateChangeActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+//add even more comments
 //added more comments
 public class StateChangeActivity extends Activity {
 	//added comments
